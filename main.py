435da46732b08e3d635e30b93f781bf9989236fb
Tim <harvard90873@gmail.com>
2021-08-14 23:20:44 +0200

    update to four spaces
    this is the primary reason why I hate python
    
    i can't code anything in 4 spaces
    
    don't yell at me
    
    it's just a personal preference!!!

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
 from sys import argv
 
+
 def main():
-  if len(argv) < 2:
-    print("Invalid command line argument")
-    return
+    if len(argv) < 2:
+        print("Invalid command line argument")
+        return
 
-  try:
-    n = int(argv[1])
-  except:
-    print("Invalid command line argument")
-    return
+    try:
+        n = int(argv[1])
+    except:
+        print("Invalid command line argument")
+        return
 
-  count = 0
-  highest = 0
+    count = 0
+    highest = 0
 
-  while n != 1:
-    highest = max(highest, n)
-    if n % 2 == 0:
-      n /= 2
-    else:
-      n = 3 * n + 1
-    count += 1
+    while n != 1:
+        highest = max(highest, n)
+        if n % 2 == 0:
+            n /= 2
+        else:
+            n = 3 * n + 1
+        count += 1
 
-  print(f"""
+    print(
+        f"""
   Stopping time = {count}
   Highest number reached = {highest}
-  """)
+  """
+    )
+
 
 if __name__ == "__main__":
-  main()
+    main()
