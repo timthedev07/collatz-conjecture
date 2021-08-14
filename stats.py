9af7426fc4e1434fc99101589d898176fc85a15c
Tim <harvard90873@gmail.com>
2021-08-14 23:44:18 +0200

    empty main function + utility function

---
+++ stats.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# this program computes the frequency of each digit being a leading digit among all computed hailstone numbers
+def main():
+    pass
+
+
+"""Generate an array of numbers containing all the hailstone numbers in the sequence"""
+
+
+def collatzArr(n: int):
+    arr = []
+    while n != 1:
+        arr.append()
+        if n % 2 == 0:
+            n /= 2
+        else:
+            n = 3 * n + 1
+    return
+
+
+if __name__ == "__main__":
+    main()
