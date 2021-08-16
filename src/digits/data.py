e3b368e604c946d0d6f85f9d8a647d0dda19a65a
Tim <harvard90873@gmail.com>
2021-08-16 23:46:51 +0200

    save original data as well

--- src/digits/data.py
+++ src/digits/data.py
@@ -29,11 +29,12 @@ def main():
             data[int(hailstoneNumber[0])] += 1
             totalNums += 1
 
-    percentages = {k: (lambda x: f"{round((x / totalNums) * 100, 2)}%\n")(v) for k, v in data.items()}
+    percentages = {k: (lambda x: f"{round((x / totalNums) * 100, 2)}%")(v) for k, v in data.items()}
 
     with open("../../out/digits.txt", "w") as f:
-        f.writelines(list(map((lambda kv: f"Digit {kv[0]}: {kv[1]}"), percentages.items())))
+        f.writelines(list(map((lambda kv: f"Digit {kv[0]}: {kv[1]}\n"), percentages.items())))
         f.write("\nOriginal data:\n")
+        f.writelines(list(map((lambda kv: f"Digit {kv[0]}: {kv[1]}\n"), data.items())))
 
 
 if __name__ == "__main__":
