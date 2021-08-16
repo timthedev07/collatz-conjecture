e77e3f0f059a5a49baaaa6167b5b2fd1254c7527
Tim <harvard90873@gmail.com>
2021-08-17 00:12:48 +0200

    plot data

--- src/digits/plot.py
+++ src/digits/plot.py
@@ -4,18 +4,22 @@ import matplotlib.pyplot as plt
 
 
 def main():
+    # frequency of numbers in range 1...9
     data = loadData("../../out/digits.txt")
+
+    plt.plot(data)
+    plt.fill_between(list(range(len(data))), data)
+    plt.show()
 
 
 """Read digits.txt"""
 
 
-def loadData(path: str) -> List[float]:
+def loadData(path: str) -> List[int]:
     with open(path, "r") as file:
         lines = file.readlines()
-        print(line for line in lines)
-
-        return []
+        nums = list(map((lambda x: int(x[9:])), lines[11:]))
+        return nums
 
 
 if __name__ == "__main__":
