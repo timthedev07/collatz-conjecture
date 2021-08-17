3c3e0efc72834e4ecc77c95af3d649fd6aa9f1a9
Tim <harvard90873@gmail.com>
2021-08-17 09:50:08 +0200

    add another function

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,17 @@
 from typing import List, Union
 
 
-"""Generate an array of numbers containing all the hailstone numbers in the sequence"""
+def collatz(n: int) -> int:
+    while n != 1:
+        if n % 2 == 0:
+            n /= 2
+        else:
+            n = 3 * n + 1
+    return n
 
 
 def collatzArr(n: int, asStr: bool = False) -> List[Union[int, str]]:
+    """Generate an array of numbers containing all the hailstone numbers in the sequence"""
     arr = []
     while n != 1:
         arr.append(n)
